jkraemer/syslog_logger

commit a9bd74340413875df8c41bff30f4f5594e75fcb9

Split messages on \n

lib/syslog_logger.rb
@@ -64,7 +64,9 @@ class SyslogLogger
         return true if #{LOGGER_LEVEL_MAP[meth]} < @level
         message ||= yield if block_given?
         if message
-          SYSLOG.#{LOGGER_MAP[meth]} clean(message)
+          message.split("\n").each do |line|
+            SYSLOG.#{LOGGER_MAP[meth]} clean(line)
+          end
         end
         return true
       end
@@ -105,7 +107,9 @@ class SyslogLogger
     severity ||= Logger::UNKNOWN
     return true if severity < @level
     message = clean(message || block.call)
-    SYSLOG.send LEVEL_LOGGER_MAP[severity], clean(message)
+    message.split("\n").each do |line|
+      SYSLOG.send LEVEL_LOGGER_MAP[severity], clean(line)
+    end
     return true
   end
 

commit 6bf8ef45b2782f7238ba6044df9acb74143cb39e

Teach SyslogLogger to gracefully handle Array messages
Loggers are only documented to handle Strings and Exceptions as
messages, but for (approximate) compatibility with the standard Logger
class, we call #to_s on messages.

This fixes that compatibility, broken in
a9bd74340413875df8c41bff30f4f5594e75fcb9. This breakage was revealed
by another change in 743cc53370fbe998096816ee7d66f5bafd7be7e1.

Change-Id: Ic64289fdd3302a888115818a2f327471229804f0
Reviewed-on: https://gerrit.causes.com/11727
Reviewed-by: Lann Martin <lann@causes.com>
Tested-by: Lann Martin <lann@causes.com>

lib/syslog_logger.rb
@@ -64,9 +64,9 @@ class SyslogLogger
         return true if #{LOGGER_LEVEL_MAP[meth]} < @level
         message ||= yield if block_given?
         if message
-          message = message.dup.force_encoding('binary')
+          message = clean(message).force_encoding('binary')
           message.split("\n").each do |line|
-            SYSLOG.#{LOGGER_MAP[meth]} clean(line)
+            SYSLOG.#{LOGGER_MAP[meth]} line
           end
         end
         return true
@@ -107,9 +107,9 @@ class SyslogLogger
   def add(severity, message = nil, progname = nil, &block)
     severity ||= Logger::UNKNOWN
     return true if severity < @level
-    message = clean(message || block.call)
+    message = clean(message || block.call).force_encoding('binary')
     message.split("\n").each do |line|
-      SYSLOG.send LEVEL_LOGGER_MAP[severity], clean(line)
+      SYSLOG.send LEVEL_LOGGER_MAP[severity], line
     end
     return true
   end
@@ -141,4 +141,3 @@ class SyslogLogger
   end
 
 end
-